7ee2a3d1c10c1dbacfe7f76f9765c1de6a6f95ed
Yaran Nan <y4nan@uwaterloo.ca>
2018-11-05 15:47:19 -0500

    Add return code handle for login activity

--- AndroidClient/AS-master/app/src/main/java/com/example/think/eduhelper/LoginDialog.java
+++ AndroidClient/AS-master/app/src/main/java/com/example/think/eduhelper/LoginDialog.java
@@ -31,22 +31,31 @@ import java.util.List;
 
 public class LoginDialog extends DialogFragment {
     private EditText name,password;
+    private android.app.Activity mActivity;
     @NonNull
+    @Override
+    public void onAttach(android.app.Activity activity){
+        super.onAttach(activity);
+        mActivity = activity;
+    }
     @Override
     public Dialog onCreateDialog(@Nullable final Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater layoutInflater = getActivity().getLayoutInflater();
         builder.setView(layoutInflater.inflate(R.layout.login_dialog,null));
-;
+
         builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+
                 Dialog dialogView = (Dialog) dialog;
                 name = dialogView.findViewById(R.id.editText_LoginName);
                 password = dialogView.findViewById(R.id.editText_LoginPassword);
                 String userName = name.getText().toString();
                 String userPassword = password.getText().toString();
 
+                //startActivity(new Intent(getContext(),AccountPage.class));
+                //Toast.makeText(getActivity(),"Login successfully!", Toast.LENGTH_SHORT).show();
                 Log.d("name",name.getText().toString());
                 Map<String, String> params = new HashMap<String, String>();
                 params.put("userName", userName);
@@ -56,11 +65,28 @@ public class LoginDialog extends DialogFragment {
                 HttpHelper.getInstance().postData(parameter, "login", new HttpCallback() {
                     @Override
                     public void postDataCallback(String Data) {
-                    }
+                        //Check if the login is correct
+                        final String jsonResponse = Data.replaceAll("[^0-9]", "");
+                        mActivity.runOnUiThread(new Runnable() {
+                            @Override
+                            //Check if the username or email exits
+                            public void run() {
+                                if (jsonResponse.equals("5000")) {
+                                    Toast.makeText(mActivity, "Loading your profile...", Toast.LENGTH_LONG).show();
 
+                                    startActivity(new Intent(getContext(),AccountPage.class));
+
+                                } else if (jsonResponse.equals("5001")) {
+                                    Toast.makeText(mActivity, "The username or password aren't correct.", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(mActivity, "System has problem. Please try again!", Toast.LENGTH_LONG).show();
+                                    name.setText("");
+                                    password.setText("");
+                                }
+                            }
+                        });
+                    }
                 });
-                name.setText("");
-                password.setText("");
 
             }
         });
