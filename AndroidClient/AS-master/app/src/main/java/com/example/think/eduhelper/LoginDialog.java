730f5851aabbf48c80c9c700e893de4014b1bac2
sggkjihua <sggkjihua@163.com>
2018-11-07 13:00:57 +0800

    login dialog useless

--- AndroidClient/AS-master/app/src/main/java/com/example/think/eduhelper/LoginDialog.java
+++ AndroidClient/AS-master/app/src/main/java/com/example/think/eduhelper/LoginDialog.java
@@ -1,9 +1,8 @@
 package com.example.think.eduhelper;
 
-import android.accounts.Account;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.arch.persistence.room.Room;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,52 +10,80 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.think.eduhelper.HttpClient.HttpCallback;
-import com.example.think.eduhelper.HttpClient.HttpHelper;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import android.util.Log;
-
-import com.example.think.eduhelper.UserLoginDB.User;
-
-import org.json.JSONObject;
-import java.util.List;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginDialog extends DialogFragment {
+
+    // now useless~~~~~~~~
     private EditText name,password;
-    private android.app.Activity mActivity;
+    private static int SIGN_IN_REQUEST_CODE = 1;
+    private ProgressDialog mProgressDialog;
+
+    FirebaseAuth mAuth;
+
     @NonNull
-    @Override
-    public void onAttach(android.app.Activity activity){
-        super.onAttach(activity);
-        mActivity = activity;
-    }
     @Override
     public Dialog onCreateDialog(@Nullable final Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater layoutInflater = getActivity().getLayoutInflater();
         builder.setView(layoutInflater.inflate(R.layout.login_dialog,null));
+        // get the firebase auth
+        mAuth = FirebaseAuth.getInstance();
 
+        mProgressDialog = new ProgressDialog(getActivity());
+        mProgressDialog.setTitle(getString(R.string.loading));
+        mProgressDialog.setMessage(getString(R.string.please_wait));
+        mProgressDialog.setIndeterminate(true);
         builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
                 Dialog dialogView = (Dialog) dialog;
                 name = dialogView.findViewById(R.id.editText_LoginName);
                 password = dialogView.findViewById(R.id.editText_LoginPassword);
                 String userName = name.getText().toString();
                 String userPassword = password.getText().toString();
 
-                //startActivity(new Intent(getContext(),AccountPage.class));
-                //Toast.makeText(getActivity(),"Login successfully!", Toast.LENGTH_SHORT).show();
-                Log.d("name",name.getText().toString());
+
+                // using fire base to skip the backend
+                if(mAuth.getCurrentUser() == null){
+                    mAuth.signInWithEmailAndPassword(userName, userPassword)
+                            .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>(){
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        // FirebaseUser user = mAuth.getCurrentUser();
+                                        // startActivity(new Intent(getContext(),AccountPage.class));
+                                        Toast.makeText(getActivity(), "Authentication successfully.", Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(getActivity(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    // ...
+                                }
+                            });
+                    /*List<AuthUI.IdpConfig> providers = Arrays.asList(
+                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                            new AuthUI.IdpConfig.PhoneBuilder().build(),
+                            new AuthUI.IdpConfig.GoogleBuilder().build());
+                    startActivityForResult( AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),SIGN_IN_REQUEST_CODE); */
+                }else{
+                    Toast.makeText(getActivity(), "Welcome " +FirebaseAuth.getInstance().getCurrentUser().getEmail(),Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getContext(),AccountPage.class));
+                }
+                // backend do not edit
+                /*Log.d("name",name.getText().toString());
                 Map<String, String> params = new HashMap<String, String>();
                 params.put("userName", userName);
                 params.put("userPassword", userPassword);
@@ -65,28 +92,11 @@ public class LoginDialog extends DialogFragment {
                 HttpHelper.getInstance().postData(parameter, "login", new HttpCallback() {
                     @Override
                     public void postDataCallback(String Data) {
-                        //Check if the login is correct
-                        final String jsonResponse = Data.replaceAll("[^0-9]", "");
-                        mActivity.runOnUiThread(new Runnable() {
-                            @Override
-                            //Check if the username or email exits
-                            public void run() {
-                                if (jsonResponse.equals("5000")) {
-                                    Toast.makeText(mActivity, "Loading your profile...", Toast.LENGTH_LONG).show();
-
-                                    startActivity(new Intent(getContext(),AccountPage.class));
-
-                                } else if (jsonResponse.equals("5001")) {
-                                    Toast.makeText(mActivity, "The username or password aren't correct.", Toast.LENGTH_LONG).show();
-                                } else {
-                                    Toast.makeText(mActivity, "System has problem. Please try again!", Toast.LENGTH_LONG).show();
-                                    name.setText("");
-                                    password.setText("");
-                                }
-                            }
-                        });
                     }
-                });
+
+                });*/
+                name.setText("");
+                password.setText("");
 
             }
         });
@@ -99,19 +109,5 @@ public class LoginDialog extends DialogFragment {
             }
         });
         return builder.create();
-    }
-
-    public Boolean ifMatch(String name, String password){
-        List<User> users= MainActivity.userInfoDatabase.userDao().getUsers();
-
-        for(User user:users){
-            if (user.getName().equals(name)){
-                if (user.getPassword().equals(password)) {
-                    return true;
-                }
-                return false;
-            }
-        }
-        return false;
     }
 }
